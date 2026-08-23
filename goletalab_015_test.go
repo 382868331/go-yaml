80157d69a31a94b94cbb3a99feab1aa638a8c8d4
382868331 <382868331@users.noreply.github.com>
2026-08-24 04:07:25 +0800

    fix: restore 单引号偏好开关 contract

--- goletalab_015_test.go
+++ goletalab_015_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestGoletaYAML015(t *testing.T){
  e:=&Encoder{};_ = UseSingleQuote(true)(e);if !e.singleQuote{t.Fatal("single quote preference was not enabled")}
 }
+
+func TestGoletaYAML015AdjacentBoundary(t *testing.T) {
+	// Exercise a distinct adjacent boundary of the same public contract.
+ e:=&Encoder{singleQuote:true};_ = UseSingleQuote(false)(e);if e.singleQuote{t.Fatal("single quote preference was not disabled")}
+}
